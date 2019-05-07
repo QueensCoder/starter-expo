fd992f5bd233f72479e61bb8fdf6fe586ae585b6
Ozal Khan <ozalkhan196@gmail.com>
2019-05-06 20:48:02 -0400

    user is now able to change position of view by dragging items in the screen

--- src/Deck.js
+++ src/Deck.js
@@ -6,13 +6,20 @@ export class Deck extends Component {
   constructor(props) {
     super(props);
 
+    const position = new Animated.ValueXY();
+
     const panResp = PanResponder.create({
       onStartShouldSetPanResponder: () => true, //clicks down on screen
-      onPanResponderMove: () => {}, //whenever we start to move like draggin the screen
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+        position.setValue({ x: gesture.dx, y: gesture.dy }); //panresponder takes in user data
+        //and sets the position to what the user puts in
+        //dx and dy is total lifecycle of a gesture
+      }, //whenever we start to move like draggin the screen
       onPanResponderRelease: () => {} //when the user presses and removes finger
     });
 
-    this.state = { panResp };
+    this.state = { panResp, position }; //might need to use this.postion and this.panresp
   }
 
   renderCards() {
@@ -21,7 +28,14 @@ export class Deck extends Component {
     });
   }
   render() {
-    return <View>{this.renderCards()}</View>;
+    return (
+      <Animated.View
+        style={this.state.position.getLayout()} //now the layout is effected by the position
+        {...this.state.panResp.panHandlers}
+      >
+        {this.renderCards()}
+      </Animated.View>
+    );
   }
 }
 
